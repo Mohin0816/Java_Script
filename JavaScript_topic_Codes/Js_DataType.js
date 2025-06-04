802026b978c94fef61b46d558424d3e6db7d13b6
Pathan Mohin khan <193593860+Mohin0816@users.noreply.github.com>
2025-06-04 14:52:24 +0000

    Datatypes

---
+++ JavaScript_topic_Codes/Js_DataType.js
@@ -0,0 +1,18 @@
+/*In the java script we donot need to declare the specific  datatype
+    automatically it will declare the datatype accourding to the value.
+    Example:-
+                ->If i stored the String value into the variable that will automatically change the varible string type
+                -> if i stored the numbers into variable that will automatically convert into integer type
+*/
+let Name="Mohinkhan"
+let age=26
+let result=true
+
+//to find the data type we have the "TYPE OF" key word to find the datetype of variable
+
+console.log(typeof Name);
+console.log(typeof age);
+console.log(typeof result);
+
+
+
