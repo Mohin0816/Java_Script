63163e362d7a3f1144998d7ada1e9b157c81524d
Pathan Mohin khan <193593860+Mohin0816@users.noreply.github.com>
2025-06-04 12:17:45 +0000

    Variables

---
+++ JavaScript_topic_Codes/Js_Variables.js
@@ -0,0 +1,24 @@
+// in javaScript we can use the variables in 4 ways
+
+Name="Mohin khan" // we can able to use the variable without any keyword But it is not Good way
+
+var Name1="Mohinkhan "    // VAR if use this var key word we can access that variable every where
+
+let age=26     //If we use the LET keyword we can use that variable with in ablock only.
+
+/* if we use this CONST key word  we can not able to change the value after once initilizing
+    the value for the 1st time
+*/
+const Aadhar=378430088763  
+
+//------------------------------------------->
+
+// CONSOLE.LOG() method is used for the print the value in console
+console.log(Name1)
+
+//CONSLE.TABLE() method is used for the printing the  multiple values at a time in console in table formate
+
+console.table([Name,Name1,age,Aadhar])
+
+
+
