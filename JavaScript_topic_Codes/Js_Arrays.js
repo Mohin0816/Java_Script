a92736e3f256acbde8360b17230d51bfc71a81b7
Pathan Mohin khan <193593860+Mohin0816@users.noreply.github.com>
2026-06-17 13:00:06 +0530

    Add array operations example in Js_Arrays.js
    Demonstrate array operations including unshift, push, pop, and shift.

---
+++ JavaScript_topic_Codes/Js_Arrays.js
@@ -0,0 +1,17 @@
+const names=["mohin","khan","hannu","sudheer"];
+console.log(names);
+
+console.log(names[2]);
+
+names.unshift("Irfan");     //To add the element to the 1st index of element.
+console.log(names);
+
+names.push("Ali");          //To add the element to the last index of array.
+console.log(names);
+
+
+names.pop();                // To remove the element from the last element from array.
+console.log(names);
+
+names.shift();              //To remove element from the 1st index of array.
+console.log(names);
